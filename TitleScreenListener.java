d944c5944d0ccccd756af86caadc00cf7bc3c594
Vincent Huang <84062739+vhcent@users.noreply.github.com>
2021-12-17 16:34:48 -0800

    Delete TitleScreenListener.java

--- TitleScreenListener.java
+++
@@ -1,8 +0,0 @@
-// import java.awt.event.ActionEvent;
-// import java.awt.event.ActionListener;
-
-// public class TitleScreenListener implements ActionListener {
-//     public void actionPerformed(ActionEvent event) {
-//         createGamePanel();
-//     }
-// }
